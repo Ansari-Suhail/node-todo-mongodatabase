e1360e05e7d12b38a157e30abe3a9c50dcf83175
Ansari Suhail Mohd Saeed <ansari.m2@ugamsolutions.com>
2017-11-16 16:48:40 +0530

    removedd

--- mongo-basics/mongoose-remove.js
+++ mongo-basics/mongoose-remove.js
@@ -3,17 +3,21 @@ const {ObjectId} = require('mongodb');
 const {mongoose} = require('./../server/db/mongoose-connection.js');
 const {computer} = require('./../server/models/computer.js');
 
-//delete all record in computer collection but does not return the object
-computer.remove({})
+//remove all records from computer collection but does not return the object
+computer.remove({}).then((success)=>{
+  console.log({success});
+},(err)=>{
+  console.log({err});
+});
 
-//delete the only one record and return the object
+//remove only one record and return the object
 computer.findOneAndRemove({password: 1111}).then((success)=>{
   console.log({success});
 },(err)=>{
   console.log({err});
 });
 
-//delete record by Id and return the object
+//remove record by Id and return the object
 computer.findOneAndRemove('5a0bfae3070a191834719e38').then((success)=>{
   console.log({success});
 },(err)=>{
